372c61d5e8d5bd5aafaf154547b7ee47fd0bc708
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2026-03-31 18:58:17 -0600

    docs(code): add professional JSDoc and english comments
    - Translated all internal comments to English for international standards.
    - Added JSDoc descriptions to the main component and methods.
    - Improved variable naming for better self-documentation.

--- src/app/component/commit-form/commit-form.ts
+++ src/app/component/commit-form/commit-form.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+/**
+ * Main editor component for CommitFlow.
+ * Handles user input and formats it into a Conventional Commit message.
+ */
 @Component({
   selector: 'app-commit-form',
   imports: [],
@@ -7,15 +11,26 @@ import { Component } from '@angular/core';
   styleUrl: './commit-form.scss',
 })
 export class CommitForm {
+  // Holds the user-provide commit description
   subject: string = '';
+
+  // Default commit type based on Conventional Commits standard
   selectedType: string = 'feat';
 
+  // List of available commit types for the selector
   commitTypes: string[] = ['feat', 'fix', 'docs', 'style', 'refactor', 'chore', 'test'];
 
+    /**
+     * Updates the subject whenever the input field changes.
+     * @param event The input event containing the new string.
+    */
     onSubjectChange(event: any) {
     this.subject = event.target.value;
   }
 
+  /**
+   * Updates the selected commit type from the dropdown menu.
+  */
   onTypeChange(event: any) {
     this.selectedType = event.target.value;
   }
