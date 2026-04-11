eea144086731051f0d5ee8d6c32589c7266e4725
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2026-04-10 19:19:44 -0600

    refactor: update commit types structure to objects and add lookup logic
    - Defined CommitType interface
    - Converted commitTypes array to include labels and descriptions
    - Implemented onTypeChange logic to support contextual help

--- src/app/component/commit-form/commit-form.ts
+++ src/app/component/commit-form/commit-form.ts
@@ -6,13 +6,22 @@ import { NgIf } from "../../../../node_modules/@angular/common/types/_common_mod
  * Main editor component for CommitFlow.
  * Handles user input and formats it into a Conventional Commit message.
  */
+
+  interface CommitType {
+    value: string;
+    label: string;
+    description: string;
+  }
+
 @Component({
   selector: 'app-commit-form',
   imports: [FormsModule],
   templateUrl: './commit-form.html',
   styleUrl: './commit-form.scss',
 })
+
 export class CommitForm {
+
   // Holds the user-provide commit description
   subject: string = '';
 
@@ -21,9 +30,19 @@ export class CommitForm {
   // Default commit type based on Conventional Commits standard
   selectedType: string = 'feat';
 
-  // List of available commit types for the selector
-  commitTypes: string[] = ['feat', 'fix', 'docs', 'style', 'refactor', 'chore', 'test'];
+  selectedTypeObject: CommitType | null = null;
 
+  // List of available commit types for the selector
+  commitTypes: CommitType[] = [
+    { value: 'feat', label: 'Feature', description: 'Introduce una nueva funcionalidad al código.' },
+    { value: 'fix', label: 'Fix', description: 'Corrige un error o fallo técnico (bug).' },
+    { value: 'docs', label: 'Documentation', description: 'Cambios solo en la documentación.' },
+    { value: 'style', label: 'Style', description: 'Cambios de formato y estética sin afectar la lógica.' },
+    { value: 'refactor', label: 'Refactor', description: 'Mejora del código que no corrige errores ni añade funciones.' },
+    { value: 'perf', label: 'Performance', description: 'Cambio que mejora el rendimiento del sistema.' },
+    { value: 'test', label: 'Test', description: 'Añadir o corregir pruebas unitarias.' },
+    { value: 'chore', label: 'Chore', description: 'Mantenimiento, herramientas o actualización de librerías.' }
+  ];
     /**
      * Updates the subject whenever the input field changes.
      * @param event The input event containing the new string.
@@ -32,11 +51,10 @@ export class CommitForm {
     this.subject = event.target.value;
   }
 
-  /**
-   * Updates the selected commit type from the dropdown menu.
-  */
-  onTypeChange(event: any) {
-    this.selectedType = event.target.value;
+  onTypesChange(): void {
+    const found = this.commitTypes.find(t => t.value === this.selectedType);
+
+    this.selectedTypeObject = found || null;
   }
 
   get fullComand(): string {
